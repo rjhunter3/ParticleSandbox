176637b5caf0c4b1d14ef613db620f7ad6ea0064
rjhunter3 <robertjh30@gmail.com>
2019-01-02 15:21:58 +0000

    Made a start on HTML Form Controls
    Now need to tidy up and link it to JS

--- ParticleSandbox-cxww44/index.js
+++ ParticleSandbox-cxww44/index.js
@@ -9,9 +9,9 @@ function setup() {
     c = new particle(particleCount, particles, globalHue)
     c.reset(particleCount, particles);
 
-    var gui = new dat.GUI();
-    gui.add(c, 'name');
-    gui.add(c, 'switch');
+    //var gui = new dat.GUI();
+    //gui.add(c, 'name');
+    //gui.add(c, 'switch');
 }
 function draw() {
     c.draw(particleCount, particles)
